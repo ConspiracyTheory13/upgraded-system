fd04da6cf4522e863d6e1e0605b6cd353773c9b5
ConspiracyTheory13 <33845593+ConspiracyTheory13@users.noreply.github.com>
2018-01-03 10:38:25 -0800

    Add files via upload

--- skinquiz.js
+++ skinquiz.js
@@ -1,93 +1,6 @@
- (function() {
-   function buildQuiz() {
-     const output = []
-
-     myQuestions.forEach((currentQuestion, questionNumber) => {
-       const answers = []
-
-       for (letter in currentQuestion.answers) {
-         answers.push(
-           `<label>
-                <input type="radio" name="question${questionNumber}" value="${letter}">
-                ${letter} :
-                ${currentQuestion.answers[letter]}
-              </label>`
-         );
-       }
-       output.push(
-         `<div class="slide">
-    <div class="question"> ${currentQuestion.question}
-    <div class="answers"> ${answers.join("")} </div>
-      </div>`
-       );
-     });
-
-     quizContainer.innerHTML = output.join("");
-   };
-
-   function showResults() {};
-   const answerContainer = quizContainer.querySelectorAll(".answers");
-   let numCorrect = 0;
-   // num correct is just a way of tracking which zone level product
-
-   myQuestions.forEach((currentQuestion, questionNumber) => {
-     const answerContainer = answerContainers[questionNumber];
-     const selector = `input[name=question${questionNumber}]:checked`;
-     const userAnswer = (answerContainer.querySelector(selector) || {})
-       .value;
-   })
-
-   if (userAnswer === currentQuestion.correctAnswer) {
-     numCorrect++;
-
-   }
-   // function showSlide(n) {
-   //   slides[currentSlide].classList.remove("active-slide");
-   //   slides[n].classList.add("active-slide");
-   //   currentSlide = n;
-   //
-   //   if (currentSlide === 0) {
-   //     Rerun
-   //     previousButton.style.display = "none";
-   //   } else {
-   //     previousButton.style.display = "inline-block";
-   //   }
-   //
-   //   if (currentSlide === slides.length - 1) {
-   //     nextButton.style.display = "none";
-   //     submitButton.style.display = "inline-block";
-   //   } else {
-   //     nextButton.style.display = "inline-block";
-   //     submitButton.style.display = "none";
-   //   }
-   // }
-   //
-   // function showNextSlide() {
-   //   console.log("Your mom");
-   //   showSlide(currentSlide + 1);
-   //
-   //   function showPreviousSlide() {
-   //     showSlide(currentSlide - 1);
-   //   }
-   // //when user selects answer point total is added to running score
-
-   const quizContainer = document.getElementById('quiz');
-   const resultsContainer = document.getElementById('results');
-   const submitButton = document.getElementById('submit');
-
-   buildQuiz();
-   const previousButton = document.getElementById("previous");
-   const nextButton = document.getElementById("next");
-   const slides = document.querySelectorAll(".slide");
-   let currentSlide = 0;
-
-   showSlide(0);
-
-   submitButton.addEventListener('click', showResults);
-   previousButton.addEventListener('click', showPreviousSlide);
-   nextButton.addEventListener('click', showNextSlide);
-   const myQuestions = [{
-     question: 'Your skin tends to be: ',
+ // (function () {
+ const myQuestions = [{
+     question: 'Your skin tends to be:',
      answers: {
        a: 'Oily',
        b: 'Dry',
@@ -95,7 +8,7 @@
      },
      aAnswer: 'a',
      bAnswer: 'b',
-     cAnswer: 'c',
+     cAnswer: 'c'
    }, {
      question: 'Do you have a skin condition or is your skin sensitive or easily irritated ? ',
      answers: {
@@ -136,5 +49,101 @@
      aAnswer: 'a',
      bAnswer: 'b',
      cAnswer: 'c'
-   }, ];
- })();
+   }
+
+ ];
+
+ function buildQuiz() {
+   const output = []
+
+   myQuestions.forEach((currentQuestion, questionNumber) => {
+
+     const answers = [];
+     for (letter in currentQuestion.answers) {
+       answers.push(
+         `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+              </label>`
+       );
+     }
+     output.push(
+       `<div class="slide">
+    <div class="question"> ${currentQuestion.question}
+    <div class="answers"> ${answers.join("")} </div>
+      </div>`
+     );
+   });
+
+   quizContainer.innerHTML = output.join("");
+ }
+
+ function showResults() {};
+ const answerContainer = quizContainer.querySelectorAll(".answers");
+ let numCorrect = 0;
+ // num correct is just a way of tracking which zone level product
+
+ myQuestions.forEach((currentQuestion, questionNumber) => {
+     const answerContainer = answerContainers[questionNumber];
+     const selector = `input[name=question${questionNumber}]:checked`;
+     const userAnswer = (answerContainer.querySelector(selector) || {})
+       .value;
+
+     if (userAnswer === currentQuestion.correctAnswer) {
+       numCorrect++;
+
+     }); resultsContainer.innerHTML =
+   `${numCorrect} out of ${myQuestions.length}`;
+ }
+
+ // function showSlide(n) {
+ //   slides[currentSlide].classList.remove("active-slide");
+ //   slides[n].classList.add("active-slide");
+ //   currentSlide = n;
+ //
+ //   if (currentSlide === 0) {
+ //     Rerun
+ //     previousButton.style.display = "none";
+ //   } else {
+ //     previousButton.style.display = "inline-block";
+ //   }
+ //
+ //   if (currentSlide === slides.length - 1) {
+ //     nextButton.style.display = "none";
+ //     submitButton.style.display = "inline-block";
+ //   } else {
+ //     nextButton.style.display = "inline-block";
+ //     submitButton.style.display = "none";
+ //   }
+ // }
+ //
+ // function showNextSlide() {
+ //   console.log("Your mom");
+ //   showSlide(currentSlide + 1);
+ //
+ //   function showPreviousSlide() {
+ //     showSlide(currentSlide - 1);
+ //   }
+ // //when user selects answer point total is added to running score
+
+ const quizContainer = document.getElementById('quiz');
+ const resultsContainer = document.getElementById('results');
+ const submitButton = document.getElementById('submit');
+
+ buildQuiz();
+ const previousButton = document.getElementById("previous");
+ const nextButton = document.getElementById("next");
+ const slides = document.querySelectorAll(".slide");
+ let currentSlide = 0;
+
+ showSlide(0);
+
+ submitButton.addEventListener('click', showResults);
+ previousButton.addEventListener(
+   'click', showPreviousSlide);
+ nextButton.addEventListener('click',
+   showNextSlide);
+
+
+ // }
