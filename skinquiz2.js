6d8109272e1678c16de166fc8aaa952044958b7f
ConspiracyTheory13 <33845593+ConspiracyTheory13@users.noreply.github.com>
2018-01-09 15:29:53 -0800

    Add files via upload

--- skinquiz2.js
+++ skinquiz2.js
@@ -1,52 +1,8 @@
-/*jslint browser:true */
-
+/* jslint browser:true */
 window.onload = function () {
+    'use strict';
 
-  'use strict';
-  const quizContainer = document.getElementById('quiz');
-  const resultsContainer = document.getElementById('results');
-  const submitButton = document.getElementById('submit');
-  //  test
-  const previousButton = document.getElementById('previous');
-  const nextButton = document.getElementById('next');
-  const slides = document.querySelectorAll('.slide');
-
-  let currentSlide = 0;
-
-  function showSlide (n) {
-    slides[currentSlide].classList.remove('active-slide');
-    slides[n].classList.add('active-slide');
-    currentSlide = n;
-
-    if (currentSlide === 0) {
-      previousButton.style.display = 'none';
-    }
-    else {
-      previousButton.style.display = 'inline-block';
-    }
-    if (currentSlide === slides.length - 1) {
-      nextButton.style.display = 'none';
-      submitButton.style.display = 'inline-block';
-    } else {
-      nextButton.style.display = 'inline-block';
-      submitButton.style.display = 'none';
-    }
-  }
-  showSlide(0);
-
-  function showNextSlide () {
-    showSlide(currentSlide + 1)
-  }
-
-  function showPreviousSlide () {
-    showSlide(currentSlide - 1)
-  }
-
-  previousButton.addEventListener('click', showPreviousSlide);
-  nextButton.addEventListener('click', showNextSlide);
-  //  test
-
-  const myQuestions = [{
+    const myQuestions = [{
     question: 'Your skin tends to be:',
     answers: {
       a: 'Oily',
@@ -93,6 +49,51 @@ window.onload = function () {
   }
   ]
 
+  const quizContainer = document.getElementById('quiz');
+  const resultsContainer = document.getElementById('results');
+  const submitButton = document.getElementById('submit');
+  //  test
+  const previousButton = document.getElementById('previous');
+  const nextButton = document.getElementById('next');
+  const slides = document.querySelectorAll('.slide');
+
+  let currentSlide = 0;
+
+  function showSlide (n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+
+    if (currentSlide === 0) {
+      previousButton.style.display = 'none';
+    }
+    else {
+      previousButton.style.display = 'inline-block';
+    }
+    if (currentSlide === slides.length - 1) {
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    } else {
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+  }
+
+  buildQuiz();
+  showSlide(0);
+
+  function showNextSlide () {
+    showSlide(currentSlide + 1)
+  }
+
+  function showPreviousSlide () {
+    showSlide(currentSlide - 1)
+  }
+
+  previousButton.addEventListener('click', showPreviousSlide);
+  nextButton.addEventListener('click', showNextSlide);
+  //  test
+
   function buildQuiz () {
     const output = [];
 
@@ -110,6 +111,7 @@ window.onload = function () {
           </label>`
           );
         }
+   });
 
         output.push(
           `<div class="slide">
@@ -117,12 +119,10 @@ window.onload = function () {
     <div class="answers"> ${answers.join("")} </div>
   </div>`
         );
-        output.push(
-          `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join('')} </div>`
-        );
+
       }
     );
+
     return quizContainer.innerHTML = output.join('');
   }
 
@@ -148,7 +148,6 @@ window.onload = function () {
     resultsContainer.innerHTML = numCorrect + ' out of ' + myQuestions.length;
   }
 
-  buildQuiz();
 
   submitButton.addEventListener('click', showResults);
 
