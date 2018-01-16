519bfcc81138cf154b7a62fa54aa2530bd9bd98e
Babylon <xbettybabylonx@gmail.com>
2018-01-15 17:29:54 -0800

    Struggling with displaying all buttons specified in html inside modal. I suspect this may be because the modal window is too small. Cannot seem to attach event handler 'click'. Now I am able to have one question display at a time, but I don't know if painatoin is working since none of my buttons are working. Sigh.

--- skinquiz2.js
+++ skinquiz2.js
@@ -1,58 +1,113 @@
-window.onload = function() {
+/* jslint browser:true */
+
+window.onload = function () {
+
+  'use strict';
   const quizContainer = document.getElementById('quiz');
   const resultsContainer = document.getElementById('results');
   const submitButton = document.getElementById('submit');
+  const previousButton = document.getElementById('previous');
+  const nextButton = document.getElementById('next');
 
-  const myQuestions = [
-    {
-      question: "Your skin tends to be:",
-      answers: {
-        a: "oily",
-        b: "dry",
-        c: "combination"
-      },
-      correctAnswer: "c"
+  let currentSlide = 0;
+
+    const myQuestions = [{
+    question: 'Your skin tends to be:',
+    answers: {
+      a: 'Oily',
+      b: 'Dry',
+      c: 'Combination'
     },
-    {
-      question: "What is the best site ever created?",
-      answers: {
-        a: "SitePoint",
-        b: "Simple Steps Code",
-        c: "Trick question; they're both the best"
-      },
-      correctAnswer: "c"
+    correctAnswer: 'c'
+  },
+  {
+    question: 'Does your skin tend to be sensitive or irritated?',
+    answers: {
+      a: 'Yes, I have a skin condition',
+      b: 'Yes, my skin tends to be sensitive',
+      c: 'No, my skin isn\'t sensitive'
     },
-    {
-      question: "Where is Waldo really?",
-      answers: {
-        a: "Antarctica",
-        b: "Exploring the Pacific Ocean",
-        c: "Sitting in a tree",
-        d: "Minding his own business, so stop asking"
-      },
-      correctAnswer: "d"
+    correctAnswer: 'c'
+  },
+  {
+    question: 'What type of climate is your skin commonly exposed too?',
+    answers: {
+      a: 'Dry and hot',
+      b: 'Humid',
+      c: 'Neither'
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: 'Where is Waldo really?',
+    answers: {
+      a: 'A',
+      b: 'B',
+      c: 'c'
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: 'Where is Waldo really?',
+    answers: {
+      a: 'A',
+      b: 'B',
+      c: 'c'
+    },
+    correctAnswer: 'c'
+  }
+  ]
+
+  function showSlide (n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+
+    if (currentSlide === 0) {
+      previousButton.style.display = 'none';
     }
-  ];
+    else {
+      previousButton.style.display = 'inline-block';
+    }
+    if (currentSlide === slides.length - 1) {
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    } else {
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+  }
 
-  function buildQuiz() {
+  function showNextSlide () {
+    showSlide(currentSlide + 1)
+  }
+
+  function showPreviousSlide () {
+    showSlide(currentSlide - 1)
+  }
+
+  function buildQuiz () {
     const output = [];
 
     myQuestions.forEach(
       (currentQuestion, questionNumber) => {
         const answers = [];
-        for (letter in currentQuestion.answers) {
+        for (var letter in currentQuestion.answers) {
+
           answers.push(
             `<label>
-            <input type="radio" name="question${questionNumber}" 
-  value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
+        <input type="radio" name="question${questionNumber}" value="${letter}">
+        ${letter} :
+        ${currentQuestion.answers[letter]}
+      </label>`
           );
         }
+
         output.push(
-          `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join('')} </div>`
+          `<div class="slide">
+        <div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join("")} </div>
+      </div>`
         );
       }
     );
@@ -73,8 +128,7 @@ window.onload = function() {
         numCorrect++;
 
         answerContainers[questionNumber].style.color = 'lightgreen';
-      }
-      else {
+      } else {
         answerContainers[questionNumber].style.color = 'red';
       }
     });
@@ -84,6 +138,15 @@ window.onload = function() {
 
   buildQuiz();
 
-  submitButton.addEventListener('click', showResults);
+  const slides = document.querySelectorAll('.slide');
 
-};
+  showSlide(0);
+
+  submitButton.addEventListener('onclick', showResults);
+  var x = document.getElementById("myNxt");
+  nextButton.addEventListener('onclick', showNextSlide);
+  x.getElementById("myNxt").addEventListener("click", showNextSlide);
+  //$(nextButton).on('click', function () {'onclick', showNextSlide});
+  //previousButton.addEventListener('onclick', showPreviousSlide);
+
+  };
